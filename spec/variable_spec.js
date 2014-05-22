96a4f59df7a42fd801f8e9d7c9461a10193c1365
Shiv Kumar Ganesh <GShiv.sk@gmail.com>
2014-05-22 17:10:06 +0530

    Minimal Code to get started with is completed, anyone can start with it in order to explore the JavaScript DataTypes

--- spec/variable_spec.js
+++ spec/variable_spec.js
@@ -15,7 +15,7 @@ describe("CHAPTER 1 --> VARIABLES", function() {
     });
 
     /*Code to check the variable decleration of Type String*/
-    it("Declare a variable Persons_Name and assign it a value Henery within double Quotes", function() {`
+    it("Declare a variable Persons_Name and assign it a value Henery within double Quotes", function() {
         expect(Persons_Name.toLowerCase()).toEqual("Henery");
     });
     it("Declare a variable Car_Color and assign a value red", function() {
@@ -26,5 +26,36 @@ describe("CHAPTER 1 --> VARIABLES", function() {
     });
     it("Declare a variable Persons_Pet and assign it value Dog", function() {
         expect(Persons_Pet.toLowerCase()).toEqual("dog");
+    });
+
+    /*Code to check the variable decleration of Type Boolean*/
+    it("Declare a variable amITrue and assign it a value true", function() {
+        expect(amITrue).toBeTruthly();
+    });
+    it("Declare a variable amIFalse and assign a value false", function() {
+        expect(amIFalse).toBeFalsy();
+    });
+
+    /*Code to check the variable decleration of type Object*/
+    it("Declare an Object Person", function() {
+        expect(Person).toEqual(jasmine.any(Object));
+    });
+
+    /*Code to check Array and the different values associates with it*/
+    it("Declare an Array called Colors", function() {
+        expect(Colors).toEqual(jasmine.any(Object));
+    });
+    it("Add Colors to array Colors namely ['red','green','blue']", function() {
+        expect(Colors).toContain('red');
+        expect(Colors).toContain('green');
+        expect(Colors).toContain('blue');
+    });
+
+    /*Code to check whether the variable talked about is a function*/
+    it("Declare a function called addition", function() {
+        expect(addition).toEqual(jasmine.any(Function));
+    });
+    it("Declare a function substraction", function() {
+        expect(substraction).toEqual(jasmine.any(Function));
     });
 });
