97112bc49ed7059e9e2104abef75ace6227b55d0
unknown <INVCH014@CHN-LT-020.Mecheri.in>
2014-05-23 17:30:35 +0530

    Added Karma as my test Runner and would Execute using this

--- spec/boolean_spec.js
+++ spec/boolean_spec.js
@@ -7,19 +7,20 @@ describe("CHAPTER 4 --> BOOLEAN & BOOLEAN OPERATIONS", function() {
         expect(boolTest).toBeTruthy();
     });
     it("Declate a variable called boolFalseTest and assign it a value False", function() {
-    	expect(boolFalseTest).toBeFalsy();
+        expect(boolFalseTest).toBeFalsy();
     });
 
     /**
      * This section deals with the functions associated with the instance variables
      */
-    
+
     it("Call function(toString()) on variable boolTest and assign the result to a variable boolTestResult_toString", function() {
-    	expect(boolTestResult_toString).toBe("true");
+        expect(boolTestResult_toString).toBe("true");
     });
     it("Call function(toLocaleString()) on variable boolTest and assign the result to a variable boolTestResult_toLocaleString", function() {
-    	expect(boolTestResult_toLocaleString).toBe("true")
+        expect(boolTestResult_toLocaleString).toBe("true")
     });
     it("Call function(valueOf()) on variable boolTest and assign the result to a variable boolTestResult_valueOf", function() {
-    	expect(boolTestResult_valueOf).toBe(true);
+        expect(boolTestResult_valueOf).toBe(true);
     });
+});
