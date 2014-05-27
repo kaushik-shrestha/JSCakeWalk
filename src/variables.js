374743030324ef0c73154028e878e0414725cc7c
unknown <INVCH014@CHN-LT-020.Mecheri.in>
2014-05-27 11:20:51 +0530

    Added some links and prerequisites for the variables chaper

--- src/variables.js
+++ src/variables.js
@@ -1,8 +1,12 @@
-/*
-Declaration of Variables
+/**
+ * Variables are the building block for any programming language.
+ * They help us to store values and other higher data types.
+ * JavaScript also has variables and they are in different flavour.
+ * They help us to program efficiently and effectively.
+ *
+ * For referance one could follow the below mentioned links and
+ * then continue with the exercise.
+ *
+ * 1. W3Schools (For not so adventure lovin people) http://bit.ly/1oCQcrw
+ * 2. MDN (For the adventurous ones) http://mzl.la/Rvo4Ku
  */
-
-var number = 12;
-var number1 = 15;
-var Colors = new Array();
-Colors = ['red', 'green', 'blue']
